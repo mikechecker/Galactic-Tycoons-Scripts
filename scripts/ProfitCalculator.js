e0e9a05c4765fdfbb92a663f1107b207331e1bb0
mikechecker <9422552+mikechecker@users.noreply.github.com>
2025-10-25 21:11:11 +0200

    Added Profit to Enxyclopedia

--- scripts/ProfitCalculator.js
+++ scripts/ProfitCalculator.js
@@ -2,49 +2,59 @@
 // @name     Galactic Tyocoons Profit Calculator
 // @version  0.1
 // @include  https://*.galactictycoons.com/*
+// @run-at document-end
 // ==/UserScript==
 
-// {Base{Ingrediented, Amount}}
-let shoppingList = new Map()
+let ExchangePrices = []
 
-addEventListener("click", function (event) {
-    setTimeout(async () => {
-      updateInfo()
-    }, 1000);
-});
+setTimeout(async () => {
+    const response = await fetch('https://api.g2.galactictycoons.com/public/exchange/mat-prices');
+    ExchangePrices = (await response.json()).prices;
+}, 100);
 
-async function updateInfo() {
-    const ModalDiv = document.getElementsByClassName("modal-content")
-    if (ModalDiv == undefined || ModalDiv.length == 0) {
+setInterval(async function () {
+    const response = await fetch('https://api.g2.galactictycoons.com/public/exchange/mat-prices');
+    ExchangePrices = (await response.json()).prices;
+}, 10000)
+
+setInterval(function () {
+    if (ExchangePrices.length == 0) {
         return
     }
 
-    const TitleDiv = ModalDiv[0].getElementsByClassName("modal-title")
+    updateEncyclpediaInfo()
+    updateProductionInfo()
+}, 100)
 
-    if (TitleDiv == undefined || TitleDiv.length == 0) {
+async function updateProductionInfo() {
+    let RecipeTbody = document.querySelector('tbody[data-tutorial="recipe-select"]')
+    if (!RecipeTbody) {
         return
     }
 
-    if (TitleDiv[0].firstChild.data.endsWith(" - Order") == false) {
+    let RecipeSelectionHeader = RecipeTbody.parentElement.querySelector('thead tr')
+
+    if (RecipeSelectionHeader.children.length != 4) {
         return
     }
 
-    TitleDiv[0].firstChild.data += " Improved"
-
-
-    const Rows = ModalDiv[0].querySelectorAll('tr')
-
+    let Cell = RecipeSelectionHeader.insertCell(-1)
     let th = document.createElement('th');
     th.textContent = "Profit"
-    Rows[0].appendChild(th);
+    Cell.appendChild(th)
 
-
-    const response = await fetch('https://api.g2.galactictycoons.com/public/exchange/mat-prices');
-    const ExchangePrices = (await response.json()).prices;
+    let RecipeRows = RecipeTbody.querySelectorAll('tr.cursor-pointer')
 
     // get all Ingredients
-    for (let i = 1; i < Rows.length; i++) {
-        const IngredientDiv = Rows[i].cells[0]
+    for (let i = 0; i < RecipeRows.length; i++) {
+        if (Row.children.length < 4) {
+            return
+        }
+        if (RecipeRows[i].children.length != 4) {
+            RecipeRows[i].removeChild(RecipeRows[i].children[4])
+        }
+
+        const IngredientDiv = RecipeRows[i].cells[0]
         const ContentDiv = IngredientDiv.getElementsByClassName("btn btn-material")
 
         let TotalCost = 0
@@ -58,10 +68,10 @@ async function updateInfo() {
             ingredients.push([ingredient, amount, price])
         }
 
-        const TimeCell = Rows[i].cells[2]
+        const TimeCell = RecipeRows[i].cells[2]
         const Time = ConvertTimeToHours(TimeCell.textContent)
 
-        const ResultCell = Rows[i].cells[3]
+        const ResultCell = RecipeRows[i].cells[3]
         const ResultContentDiv = ResultCell.getElementsByClassName("btn btn-material")
 
         const amount = ResultContentDiv[0].getElementsByClassName("badge btn-badge bg-secondary")[0].textContent
@@ -72,9 +82,72 @@ async function updateInfo() {
 
         let td = document.createElement('td');
         td.textContent = Math.round((price * amount - TotalCost) / Time / 100).toLocaleString() + "$"
-        Rows[i].appendChild(td);
+        RecipeRows[i].appendChild(td);
+    }
+}
+
+async function updateEncyclpediaInfo() {
+    let SourceDiv = document.querySelector('div[class="fw-bold"]')
+    if (!SourceDiv || SourceDiv.textContent != "Source") {
+        return
     }
 
+    SourceDiv.parentElement.querySelectorAll('thead tr').forEach(tr => {
+        if (tr.children.length < 4) {
+            return
+        }
+
+        if (tr.children.length == 5) {
+            let Cell = tr.insertCell(-1)
+            let newTh = document.createElement('th');
+            newTh.textContent = "Profit"
+            Cell.appendChild(newTh)
+        }
+
+        let RecipeRows = tr.parentElement.parentElement.querySelectorAll('tbody tr')
+
+        // get all Ingredients
+        for (let i = 0; i < RecipeRows.length; i++) {
+            let Row = RecipeRows[i]
+            if (Row.children.length < 5) {
+                return
+            }
+
+            if (Row.children.length == 6) {
+                Row.removeChild(Row.children[5])
+            }
+
+            const IngredientDiv = Row.cells[1]
+            const ContentDiv = IngredientDiv.getElementsByClassName("btn btn-material")
+
+            let TotalCost = 0
+            let ingredients = []
+            for (var Recipe of ContentDiv) {
+                const amount = Recipe.getElementsByClassName("badge btn-badge bg-secondary")[0].textContent
+                const ingredient = Recipe.getElementsByClassName("btn-caption")[0].textContent
+                const price = GetMat(ingredient, ExchangePrices).avgPrice
+                TotalCost += amount * price
+
+                ingredients.push([ingredient, amount, price])
+            }
+
+            const TimeCell = Row.cells[3]
+            const Time = ConvertTimeToHours(TimeCell.textContent)
+
+            const ResultCell = Row.cells[4]
+            const ResultContentDiv = ResultCell.getElementsByClassName("btn btn-material")
+
+            const amount = ResultContentDiv[0].getElementsByClassName("badge btn-badge bg-secondary")[0].textContent
+            const ingredient = ResultContentDiv[0].getElementsByClassName("btn-caption")[0].textContent
+
+            const price =
+                GetMat(ingredient, ExchangePrices).avgPrice
+
+            let td = document.createElement('td');
+            td.textContent = Math.round((price * amount - TotalCost) / Time / 100).toLocaleString() + "$"
+            Row.appendChild(td);
+        }
+    })
 }
 
 function ConvertTimeToHours(_time) {
